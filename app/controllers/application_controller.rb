0d02defadcbcfdc59787021d7d8d8600ecea5397
dekohiroki44 <drinkingrider@hotmail.co.jp>
2020-04-05 19:46:06 +0900

    fix task2_5

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,3 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  before_action :request_path
-  def request_path
-    @path = "#{controller_path}##{action_name}"
-    def @path.is(*str)
-      str.map { |s| include?(s) }.include?(true)
-    end
-  end
 end
