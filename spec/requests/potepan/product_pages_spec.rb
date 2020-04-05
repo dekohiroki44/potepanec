c0d9a14adcdf132633e387e2c195d934ff51dc36
dekohiroki44 <drinkingrider@hotmail.co.jp>
2020-04-05 11:46:25 +0900

    fix task2_2

--- spec/requests/potepan/product_pages_spec.rb
+++ spec/requests/potepan/product_pages_spec.rb
@@ -2,12 +2,10 @@ require 'rails_helper'
 
 RSpec.describe 'product_pages', type: :request do
   describe "GET #show" do
-    before do
-      @product = create(:product)
-    end
+    let(:product) { create(:product) }
 
     it "returns http success" do
-      get potepan_product_path(@product.id)
+      get potepan_product_path(product.id)
       expect(response).to have_http_status(:success)
     end
   end
--- spec/systems/potepan/product_pages_spec.rb
+++ spec/systems/potepan/product_pages_spec.rb
@@ -1,17 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe 'product_pages', type: :system do
-  before do
-    @product = create(:product)
-    visit potepan_product_path(@product.id)
-  end
+  let(:product) { create(:product) }
 
   it 'is displayed correct contents' do
-    expect(page).to have_title "#{@product.name} - BIGBAG Store"
-    expect(page).to have_selector '.page-title', text: @product.name
+    visit potepan_product_path(product.id)
+    expect(page).to have_title "#{product.name} - BIGBAG Store"
+    expect(page).to have_selector '.page-title', text: product.name
     expect(page).to have_link href: potepan_index_path
-    expect(page).to have_content @product.price
-    expect(page).to have_content @product.description
-    expect(all('.item').size).to eq @product.images.count
+    expect(page).to have_content product.price
+    expect(page).to have_content product.description
+    expect(all('.item').size).to eq product.images.count
   end
 end
