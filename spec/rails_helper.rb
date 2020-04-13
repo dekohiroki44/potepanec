efec55bb4ff33088614858e11c95ae29f9fbbe29
dekohiroki44 <drinkingrider@hotmail.co.jp>
2020-04-13 17:11:52 +0900

    categories_page_specにテストを追加、product_categoryのdata-targetを変更

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,12 +1,11 @@
-# This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
-# Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
 require 'capybara/rspec'
-Dir[Rails.root.join('spec', 'support', '**', '*.rb')].each { |f| require f }
+require 'spree/testing_support/factories'
+Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
 begin
   ActiveRecord::Migration.maintain_test_schema!
 rescue ActiveRecord::PendingMigrationError => e
@@ -22,30 +21,26 @@ RSpec.configure do |config|
   config.before(:each, type: :system, js: true) do
     driven_by :selenium_chrome_headless
   end
-  # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
+
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
   config.use_transactional_fixtures = true
 
   config.infer_spec_type_from_file_location!
-  # Filter lines from Rails gems in backtraces.
+
   config.filter_rails_from_backtrace!
-  # arbitrary gems may also be filtered via:
-  # config.filter_gems_from_backtrace("gem name")
+
   config.include FactoryBot::Syntax::Methods
 end
 
-Capybara.register_driver :selenium do |app|
-  Capybara::Selenium::Driver.new(app, {
-    browser: :chrome,
-    desired_capabilities: Selenium::WebDriver::Remote::Capabilities.chrome(
-      chrome_options: {
-        # NOTE: ここでno-sandboxオプション追加する
-        args: %w[no-sandbox disable-dev-shm-usage headless disable-gpu window-size=1680,1050],
-      }
-    ),
-  })
+Capybara.register_driver :selenium_chrome_headless do |app|
+  browser_options = ::Selenium::WebDriver::Chrome::Options.new
+  browser_options.args << '--headless'
+  browser_options.args << '--no-sandbox'
+  browser_options.args << '--disable-gpu'
+  Capybara::Selenium::Driver.new(app, browser: :chrome, options: browser_options)
 end
-Capybara.javascript_driver = :selenium
 
-require 'spree/testing_support/factories'
+def take_screenshot
+  page.save_screenshot "tmp/capybara/screenshot-#{DateTime.now}.png"
+end
--- spec/systems/potepan/categories_page_spec.rb
+++ spec/systems/potepan/categories_page_spec.rb
@@ -3,8 +3,18 @@ require 'rails_helper'
 RSpec.describe 'categories_page', type: :system do
   let(:taxon) { create(:taxon) }
   let!(:product) { create(:product, taxons: [taxon]) }
-  let!(:taxonomy_test) { create(:taxonomy, name: 'Categories') }
-  let!(:taxon_test) { create(:taxon, parent_id: taxonomy_test.taxons.first.id) }
+  let!(:taxonomy_category) { create(:taxonomy, name: 'Categories') }
+  let!(:taxonomy_brand) { create(:taxonomy, name: 'Brand') }
+  let!(:taxon_shoes) do
+    create(:taxon, name: 'shoes',
+                   taxonomy: taxonomy_category,
+                   parent_id: taxonomy_category.taxons.first.id)
+  end
+  let!(:taxon_PHP) do
+    create(:taxon, name: 'PHP',
+                   taxonomy: taxonomy_brand,
+                   parent_id: taxonomy_brand.taxons.first.id)
+  end
 
   before do
     visit potepan_category_path(taxon.id)
@@ -23,10 +33,13 @@ RSpec.describe 'categories_page', type: :system do
     expect(current_path).to eq potepan_product_path(product.id)
   end
 
-  it 'show link of taxonomy or taxon in product_category' do
-    within '.product-category' do
-      click_link "#{taxonomy_test.name}"
-      expect(page).to have_link taxon_test.name, href: potepan_category_path(taxon_test.id)
+  describe 'in product cutegory', js: true do
+    it 'displayed link of taxon that belongs to taxonomy after click taxonomy ' do
+      within '.product-category' do
+        click_link "#{taxonomy_category.name}"
+        expect(page).to have_link taxon_shoes.name, href: potepan_category_path(taxon_shoes.id)
+        expect(page).not_to have_link taxon_PHP.name
+      end
     end
   end
 end
