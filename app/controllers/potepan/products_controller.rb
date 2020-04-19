b4d7fc146c24512e4851de287f826c6478291ebc
dekohiroki44 <drinkingrider@hotmail.co.jp>
2020-04-19 11:43:33 +0900

    関連商品の最大表示数を示す定数を定義

--- app/controllers/potepan/products_controller.rb
+++ app/controllers/potepan/products_controller.rb
@@ -1,6 +1,8 @@
 class Potepan::ProductsController < ApplicationController
+  MAX_DISPLAY_NUMBER = 4
+
   def show
     @product = Spree::Product.find(params[:id])
-    @related_products = @product.related_products(4)
+    @related_products = @product.related_products(MAX_DISPLAY_NUMBER)
   end
 end
