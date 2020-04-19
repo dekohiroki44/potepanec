e4091c91d877f10a98d2abe60c08e9d4dcdaa1a5
dekohiroki44 <drinkingrider@hotmail.co.jp>
2020-04-19 13:12:41 +0900

    product_controller.rbを修正

--- app/controllers/potepan/products_controller.rb
+++ app/controllers/potepan/products_controller.rb
@@ -1,6 +1,5 @@
 class Potepan::ProductsController < ApplicationController
   MAX_DISPLAY_NUMBER = 4
-
   def show
     @product = Spree::Product.find(params[:id])
     @related_products = @product.related_products(MAX_DISPLAY_NUMBER)
