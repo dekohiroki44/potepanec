86ba59abc87473fe7472cc96ead8b13a16fd2fde
dekohiroki44 <drinkingrider@hotmail.co.jp>
2020-05-05 22:13:20 +0900

    エラーハンドリングを修正

--- app/controllers/potepan/searchs_controller.rb
+++ app/controllers/potepan/searchs_controller.rb
@@ -1,21 +1,21 @@
 require 'httpclient'
 class Potepan::SearchsController < ApplicationController
   def suggest
-    url = Rails.application.credentials.api[:url]
+    url = 'https://presite-potepanec-task5.herokuapp.com/potepan/api/suggests'
     key = Rails.application.credentials.api[:api_key]
     headers = { 'Authorization': "Bearer #{key}" }
     client = HTTPClient.new
-    if params[:keyword].present?
-      query = { 'keyword': params[:keyword], 'max_num': params[:max_num] }
-      response = client.get(url, query, headers)
-      @result = JSON.parse(response.body)
-      if response.status == 200
+    query = { 'keyword': params[:keyword], 'max_num': params[:max_num] }
+    response = client.get(url, query, headers)
+    @result = JSON.parse(response.body)
+    if response.status == 200
+      if params[:keyword].present?
         render status: 200, json: @result
-      elsif response.status == 500
-        render status: 500, json: response.body
+      else
+        render status: 200, body: nil
       end
     else
-      render body: nil
+      render status: response.status, json: response.body
     end
   end
 end
